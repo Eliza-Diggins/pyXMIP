b0cb4d1c91705a6f48addb1542eb9ae46e3b1588
Nathan Diggins <eliza.diggins@utah.edu>
2024-03-19 08:46:59 -0600

    fixed issues with the setup.py script.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup
 
 setup(
-    name="pyXs",
-    packages=["pyXs"],
+    name="pyXMIP",
+    packages=["pyXMIP"],
     version="0.1.0",
     description="",
     author="Eliza C. Diggins",
     author_email="eliza.diggins@utah.edu",
-    url="https://github.com/Wik-Group/pyXs",
-    download_url="https://github.com/Wik-Group/pyXs/tarball/0.1.0",
+    url="https://github.com/eliza-diggins/pyXMIP",
+    download_url="https://github.com/eliza-diggins/pyXMIP/tarball/0.1.0",
     install_requires=["numpy", "scipy", "astropy", "astroquery", "rich"],
     classifiers=[
         "Intended Audience :: Science/Research",
