a8acd860c57d44beba7ba8654157b9822bacdfa0
Nathan Diggins <eliza.diggins@utah.edu>
2024-06-11 12:01:12 -0600

    First release (pre-release 1)

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="pyXMIP",
-    packages=["pyXMIP"],
-    version="0.1.0",
-    description="",
-    author="Eliza C. Diggins",
-    author_email="eliza.diggins@utah.edu",
-    url="https://github.com/eliza-diggins/pyXMIP",
-    download_url="https://github.com/eliza-diggins/pyXMIP/tarball/0.1.0",
-    install_requires=["numpy", "scipy", "astropy", "astroquery", "rich"],
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Visualization",
-    ],
-    include_package_data=True,
-    scripts=[
-        "scripts/pyxmip",
-    ],
-)
+setup()
